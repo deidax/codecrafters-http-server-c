28942ae44a0c879de5f2e8267e35dd8a51575388
zbouzangad <khalildida93@gmail.com>
2024-05-21 15:18:08 +0100

    Content-Encoding header

--- app/server.c
+++ app/server.c
@@ -15,6 +15,13 @@ struct HttpRequest {
     char path[100];
     char protocol[20];
 	char user_agent[BUFFER_SIZE];
+	char accepted_encoding[BUFFER_SIZE];
+	char body[BUFFER_SIZE];
+};
+
+struct HttpResponse {
+	char status[BUFFER_SIZE];
+	char content_encoding[BUFFER_SIZE];
 	char body[BUFFER_SIZE];
 };
 
@@ -22,8 +29,11 @@ char *resp_200 = "HTTP/1.1 200 OK\r\n";
 char *resp_201 = "HTTP/1.1 201 Created\r\n\r\n";
 char *resp_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
 
+char *accepted_encoding[] = {"gzip"};
+
 void initHttpRequest(struct HttpRequest *request, char *req_buffer);
-void initUserAgent(struct HttpRequest *request, char *req_buffer);
+void initHttpResponse(struct HttpResponse *response, char *status, char *content_encoding, char *body);
+void initHeader(char *header_value, size_t header_value_size, char *req_buffer, const char *header_name); 
 void processResponse(struct HttpRequest *request, int client, char *directory);
 int serverEcho(struct HttpRequest *request, int client);
 int requestUserAgent(struct HttpRequest *request, int client);
@@ -31,8 +41,11 @@ int requestFile(struct HttpRequest *request, int client, char *directory);
 int postFile(struct HttpRequest *request, int client, char *directory);
 char *readFile(char *filename);
 void writeFile(char *filename, char *content);
-char *writeResponse(char *type, char *response_body);
+char *writeResponse(char *type, struct HttpResponse *response);
 char *getDirectoryPath(int argc, char **argv);
+int checkIfExistsInArray(char *origin_a[], int origin_a_len, char *value);
+char** tokenizer(const char* str, const char* delim, int* count);
+void free_tokens(char** tokens);
 
 int main(int argc, char **argv) {
 	// Disable output buffering
@@ -116,7 +129,8 @@ void initHttpRequest(struct HttpRequest *request, char *req_buffer) {
 	char *req_body[3] = {NULL};
 	int i = 0;
 
-	initUserAgent(request, strdup(req_buffer));
+	initHeader(request->user_agent, sizeof(request->user_agent), strdup(req_buffer), "User-Agent");
+	initHeader(request->accepted_encoding	, sizeof(request->accepted_encoding	), strdup(req_buffer), "Accept-Encoding");
 
 	while ((token = strtok_r(rest, " ", &rest))){
 		req_body[i++] = token;
@@ -145,48 +159,47 @@ void initHttpRequest(struct HttpRequest *request, char *req_buffer) {
 	
 }
 
-void initUserAgent(struct HttpRequest *request, char *req_buffer) {
-	
-	char *token = NULL;
-	char *rest = req_buffer;
-	char *req_body[4] = {NULL};
-	char *user_agent[2] = {NULL};
-	
-	int i = 0;
-	while ((token = strtok_r(rest, "\r\n", &rest))){
-		req_body[i++] = token;
-		if (i >= 4)
-			break;
-	}
-	
-	for (int j=1; j <= 3; j++){
-		token = NULL;
-		rest = req_body[j];
-		i = 0;
-		if (rest != NULL){
-			while ((token = strtok_r(rest, ": ", &rest))){
-				user_agent[i++] = token;
-				if ( i >= 2 )
-					break;
-			}
-		}
-		
-		if ( user_agent[0] != NULL && strcmp(user_agent[0], "User-Agent") == 0 ){
-			strncpy(request->user_agent, user_agent[1], sizeof(request->user_agent)-1);
-			break;
-		}
+void initHttpResponse(struct HttpResponse *response, char *status, char *content_encoding, char *body) {
 
-		*user_agent = NULL;
-	}
+    strncpy(response->status, status, sizeof(response->status) - 1);
+    strncpy(response->content_encoding, content_encoding, sizeof(response->content_encoding) - 1);
+    strncpy(response->body, body, sizeof(response->body) - 1);
 
+    response->status[sizeof(response->status) - 1] = '\0';
+    response->content_encoding[sizeof(response->content_encoding) - 1] = '\0';
+    response->body[sizeof(response->body) - 1] = '\0';
 
-
-	if ( user_agent == NULL ){
-		strncpy(request->user_agent, "no-user-agent", sizeof(request->user_agent)-1);
-	}
-
-	request->user_agent[sizeof(request->user_agent)-1] = '\0';
 }
+
+void initHeader(char *header_value, size_t header_value_size, char *req_buffer, const char *header_name) {
+    char *token = NULL;
+    char *rest = req_buffer;
+    char *line = NULL;
+    char *header[2] = {NULL};
+
+    while ((line = strtok_r(rest, "\r\n", &rest))) {
+        token = strtok(line, ": ");
+        if (token) {
+            header[0] = token;
+            token = strtok(NULL, "");
+            if (token) {
+                header[1] = token;
+            } else {
+                header[1] = "";
+            }
+
+            if (strcmp(header[0], header_name) == 0) {
+                strncpy(header_value, header[1], header_value_size - 1);
+                header_value[header_value_size - 1] = '\0';
+                return;
+            }
+        }
+    }
+
+    strncpy(header_value, "no-value", header_value_size - 1);
+    header_value[header_value_size - 1] = '\0';
+}
+
 
 void processResponse(struct HttpRequest *request, int client, char *directory){
 	
@@ -237,9 +250,25 @@ int serverEcho(struct HttpRequest *request, int client){
 	int echo = strcmp(echo_path[0], "echo");
 
 	if (echo == 0){
-		char *echo_response = writeResponse("text/plain", echo_path[1]);
+
+		struct HttpResponse response;
+		initHttpResponse(&response, resp_200, "", echo_path[1]);
+
+		printf(".....%s\n", response.body);
+
+		int count = 0;
+		char **accepted_encodings = tokenizer(request->accepted_encoding, ", ", &count);
+
+		// check first accepted encoding for now, later on we could check multiple encodings
+		if ( checkIfExistsInArray(accepted_encoding, sizeof(accepted_encoding) / sizeof(accepted_encoding[0]), accepted_encodings[0]) == 1){
+			printf("ENCODING...FOND...%s\n", request->accepted_encoding);
+			strcpy(response.content_encoding, accepted_encodings[0]);
+		}
+
+		char *echo_response = writeResponse("text/plain", &response);
 
 		if (echo_response != NULL){
+
 			send(client, echo_response, strlen(echo_response),0);
 			free(echo_response);
 		}
@@ -265,7 +294,11 @@ int requestUserAgent(struct HttpRequest *request, int client){
 
 	int user_agent = strcmp(user_agent_path[0], "user-agent");
 	if (user_agent == 0){
-		char *user_agent_response = writeResponse("text/plain", request->user_agent);
+
+		struct HttpResponse response;
+		initHttpResponse(&response, resp_200, "", request->user_agent);
+
+		char *user_agent_response = writeResponse("text/plain", &response);
 
 		if (user_agent_response != NULL){
 			send(client, user_agent_response, strlen(user_agent_response),0);
@@ -300,7 +333,10 @@ int requestFile(struct HttpRequest *request, int client, char *directory){
 		snprintf(dir_file, BUFFER_SIZE, "%s/%s", directory, file_path[1]);
 		printf("directory %s\n", dir_file);
 
-		char *file_response = writeResponse("application/octet-stream", dir_file);
+		struct HttpResponse response;
+		initHttpResponse(&response, resp_200, "", dir_file);
+
+		char *file_response = writeResponse("application/octet-stream", &response);
 		free(dir_file);
 		if (file_response != NULL){
 			send(client, file_response, strlen(file_response),0);
@@ -348,25 +384,33 @@ int postFile(struct HttpRequest *request, int client, char *directory){
 	return 0;
 }
 
-char *writeResponse(char *type, char *response_body){
+char *writeResponse(char *type, struct HttpResponse *response){
 
-	if (response_body == NULL){
+	if (response->body == NULL){
 		return NULL;
 	}
 
 	char *buffer = (char *)malloc(BUFFER_SIZE);
 
-	if( buffer == NULL ){
-		printf("SERVER ERROR");
-		return NULL;
-	}
+	if (buffer == NULL) {
+        printf("SERVER ERROR");
+        return NULL;
+    }
 
 	if (strcmp(type, "text/plain") == 0){
-		size_t body_len = strlen(response_body);
+		size_t body_len = strlen(response->body);
 		size_t len = 0;
-
+		size_t cnt_len = 0;
+		
 		if (body_len > 0){
-			len = snprintf(buffer, BUFFER_SIZE,"%sContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s",resp_200, body_len, response_body);
+			
+			if (response->content_encoding[0] != '\0'){
+				len = snprintf(buffer, BUFFER_SIZE,"Content-Encoding: %s\r\n%sContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", response->content_encoding, response->status, body_len, response->body);
+			}
+			else{
+				len = snprintf(buffer, BUFFER_SIZE,"%sContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", response->status, body_len, response->body);
+			}
+
 		}
 		
 		if (len < 0 || len >= BUFFER_SIZE) {
@@ -377,8 +421,8 @@ char *writeResponse(char *type, char *response_body){
 	}
 
 	else if (strcmp(type, "application/octet-stream") == 0){
-		printf("READING FILE %s...", response_body);
-		char *file_data = readFile(response_body);
+		printf("READING FILE %s...", response->body);
+		char *file_data = readFile(response->body);
 
 		if (file_data == NULL){
 			return NULL;
@@ -389,7 +433,7 @@ char *writeResponse(char *type, char *response_body){
 		size_t len = 0;
 
 		if (body_len > 0){
-			len = snprintf(buffer, BUFFER_SIZE,"%sContent-Type: application/octet-stream\r\nContent-Length: %zu\r\n\r\n%s",resp_200, body_len, file_data);
+			len = snprintf(buffer, BUFFER_SIZE,"%sContent-Type: application/octet-stream\r\nContent-Length: %zu\r\n\r\n%s",response->status, body_len, file_data);
 		}
 		
 		if (len < 0 || len >= BUFFER_SIZE) {
@@ -465,4 +509,71 @@ char *getDirectoryPath(int argc, char **argv) {
         }
     }
     return NULL;
+}
+
+int checkIfExistsInArray(char *origin_a[], int origin_a_len, char *value)
+{
+	for (int i = 0; i < origin_a_len; i++){
+		if (strcmp(origin_a[i], value) == 0)
+			return 1;
+	}
+
+	return 0;
+}
+
+char** tokenizer(const char* str, const char* delim, int* count) {
+    char* str_copy = strdup(str);
+    if (str_copy == NULL) {
+        perror("strdup");
+        exit(EXIT_FAILURE);
+    }
+
+    int token_count = 0;
+    char* token = strtok(str_copy, delim);
+    while (token != NULL) {
+        token_count++;
+        token = strtok(NULL, delim);
+    }
+
+    char** tokens = malloc((token_count + 1) * sizeof(char*));
+    if (tokens == NULL) {
+        perror("malloc");
+        free(str_copy);
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(str_copy, str);
+
+    token_count = 0;
+    token = strtok(str_copy, delim);
+    while (token != NULL) {
+        tokens[token_count] = strdup(token);
+        if (tokens[token_count] == NULL) {
+            perror("strdup");
+            for (int i = 0; i < token_count; i++) {
+                free(tokens[i]);
+            }
+            free(tokens);
+            free(str_copy);
+            exit(EXIT_FAILURE);
+        }
+        token_count++;
+        token = strtok(NULL, delim);
+    }
+    tokens[token_count] = NULL;
+
+    free(str_copy);
+
+    if (count != NULL) {
+        *count = token_count;
+    }
+
+    return tokens;
+}
+
+void free_tokens(char** tokens) {
+    for (int i = 0; tokens[i] != NULL; i++) {
+        free(tokens[i]);
+    }
+    free(tokens);
 }
